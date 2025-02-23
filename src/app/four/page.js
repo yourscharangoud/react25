742b039b89890cb1a0608d77578d0d52edfdc239
Charan <yourscharangoud@gmail.com>
2025-02-23 21:24:20 +0530

    mfix

--- src/app/four/page.js
+++ src/app/four/page.js
@@ -1,6 +1,6 @@
 export default function Four() {
     return (
-      <div className="md:ml-60 p-6 mt-16 md:mt-0 transition-all duration-300">
+      <div className="md:ml-60 p-6 mt-10 md:mt-0 transition-all duration-300">
         <h1 className="text-3xl font-bold">Four</h1>
       </div>
     );
